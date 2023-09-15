7ce2f7fb2f7b805f1088f1e2f4048bb9d13c9fc0
Frank Pang <frankpang@microsoft.com>
2023-09-15 13:29:45 -0700

    revert unnecessary changes to schema.go

--- main/schema.go
+++ main/schema.go
@@ -11,76 +11,77 @@ import (
 
 const (
 	publicSettingsSchema = `{
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "Application Health - Public Settings",
-    "type": "object",
-    "properties": {
-      "protocol": {
-        "description": "Required - can be 'tcp', 'http', or 'https'.",
-        "type": "string",
-        "enum": ["tcp", "http", "https"]
-      },
-      "port": {
-        "description": "Required when the protocol is 'tcp'. Optional when the protocol is 'http' or 'https'.",
-        "type": "integer",
-        "minimum": 1,
-        "maximum": 65535
-      },
-      "requestPath": {
-        "description": "Path on which the web request should be sent. Required when the protocol is 'http' or 'https'.",
-        "type": "string"
-      },
-      "intervalInSeconds": {
-        "description": "Optional - The interval, in seconds, for how frequently to probe the endpoint for health status.",
-        "type": "integer",
-        "default": 5,
-        "minimum": 5,
-        "maximum": 60
-      },
-      "numberOfProbes": {
-        "description": "Optional - The number of probe reponses needed to change health state",
-        "type": "integer",
-        "default": 1,
-        "minimum": 1,
-        "maximum": 24
-      },
-      "gracePeriod": {
-        "description": "Optional - The amount of time in seconds the application will default to 'Initializing' state if no valid health state is observed numberOfProbes consecutive times.",
-        "type": "integer",
-        "minimum": 5,
-        "maximum": 14400
-      },
-      "vmWatchSettings": {
-        "description": "Optional - VMWatch plugin settings",
-        "type": "object",
-        "properties": {
-          "enabled": {
-            "description": "Optional - Toggles whether VMWatch plugin will be started",
-            "type": "boolean",
-            "default": false
-          },
-          "tests": {
-            "description": "Optional - Specific list of test names to run, will ignore running other tests that are not in the list",
-            "type": "array"
-          },
-          "parameterOverrides": {
-            "description": "Optional - Parameter overrides specific to VMWatch execution",
-            "type": "object",
-            "default": {}
-          }
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "Application Health - Public Settings",
+  "type": "object",
+  "properties": {
+    "protocol": {
+      "description": "Required - can be 'tcp', 'http', or 'https'.",
+      "type": "string",
+      "enum": ["tcp", "http", "https"]
+    },
+	"port": {
+	  "description": "Required when the protocol is 'tcp'. Optional when the protocol is 'http' or 'https'.",
+      "type": "integer",
+      "minimum": 1,
+      "maximum": 65535
+	},
+    "requestPath": {
+      "description": "Path on which the web request should be sent. Required when the protocol is 'http' or 'https'.",
+      "type": "string"
+    },
+    "intervalInSeconds": {
+      "description": "The interval, in seconds, for how frequently to probe the endpoint for health status.",
+      "type": "integer",
+      "default": 5,
+      "minimum": 5,
+      "maximum": 60
+    },
+    "numberOfProbes": {
+      "description": "The number of probe reponses needed to change health state",
+      "type": "integer",
+      "default": 1,
+      "minimum": 1,
+      "maximum": 24
+    },
+    "gracePeriod": {
+      "description": "The amount of time in seconds the application will default to 'Initializing' state if no valid health state is observed numberOfProbes consecutive times.",
+      "type": "integer",
+      "minimum": 5,
+      "maximum": 14400
+    },
+    "vmWatchSettings": {
+      "description": "Optional - VMWatch plugin settings",
+      "type": "object",
+      "properties": {
+        "enabled": {
+          "description": "Optional - Toggles whether VMWatch plugin will be started",
+          "type": "boolean",
+          "default": false
+        },
+        "tests": {
+          "description": "Optional - Specific list of test names to run, will ignore running other tests that are not in the list",
+          "type": "array"
+        },
+        "parameterOverrides": {
+          "description": "Optional - Parameter overrides specific to VMWatch execution",
+          "type": "object",
+          "default": {}
         }
       }
-    },
-    "additionalProperties": false
-  }`
+    }
+  },
+  "additionalProperties": false
+}`
 
 	protectedSettingsSchema = `{
-    "$schema": "http://json-schema.org/draft-04/schema#",
-    "title": "Application Health - Protected Settings",
-    "type": "object",
-    "properties": {},
-    "additionalProperties": false
-  }`
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "Application Health - Protected Settings",
+  "type": "object",
+  "properties": {
+  },
+  "additionalProperties": false
+}`
 )
 
 // validateObjectJSON validates the specified json with schemaJSON.
