81df56dae85ef90ef9c812edc2b6f1d3702749fe
Kevin Lugo Rosado <klugorosado@microsoft.com>
2023-10-27 20:32:13 -0700

    Try fix go workflow

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -2,6 +2,6 @@
 source integration-test/test/test_helper.bash
 create_certificate
 # 1_basic.bats Integration Test must be run sequentially
-sudo bats integration-test/test/1_basic.bats --pretty -T --trace
-sudo bats $(find integration-test/test/ -type f -name "*.bats" ! -name "1_basic.bats") --jobs 10 --pretty -T --trace
+sudo bats integration-test/test/1_basic.bats -T --trace
+sudo bats $(find integration-test/test/ -type f -name "*.bats" ! -name "1_basic.bats") --jobs 10 -T --trace
 delete_certificate
