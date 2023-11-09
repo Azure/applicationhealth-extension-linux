7c46ed38474365e2c7656fb9a2ebc9653cc3f56f
Kevin Lugo Rosado <klugorosado@microsoft.com>
2023-11-08 19:05:26 -0800

    capture error from run.sh script

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 source integration-test/test/test_helper.bash
 create_certificate
-# 1_basic.bats Integration Test must be run sequentially
+# Run Sequential Integration Tests
 sudo bats integration-test/test/1_basic.bats -T --trace
+err1=$?
+# Run Parallel Integration Tests
 sudo bats $(find integration-test/test/ -type f -name "*.bats" ! -name "1_basic.bats") --jobs 10 -T --trace
+err2=$?
 delete_certificate
+exit $((err1 + err2))
