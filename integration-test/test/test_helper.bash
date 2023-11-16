eea0c887e4a9339e18419937c8e054c4951adc75
Frank Pang <frankpang@microsoft.com>
2023-11-16 15:15:27 -0800

    Fix app health handler.log directory path

--- integration-test/test/test_helper.bash
+++ integration-test/test/test_helper.bash
@@ -102,7 +102,7 @@ container_read_vmwatch_log() {
 }
 
 container_read_handler_log() {
-    container_read_file /var/log/azure/Extension/VE.RS.ION/handler.log
+    container_read_file /var/log/azure/application-health/handler.log
 }
 
 mk_certs() { # creates certs/{THUMBPRINT}.(crt|key) files under ./certs/ and prints THUMBPRINT
