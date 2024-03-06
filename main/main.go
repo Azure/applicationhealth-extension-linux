d79beefb712bc08d5604f2fd609393046f259d43
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-03-06 01:27:15 +0000

    Remove unused imports and functions

--- main/main.go
+++ main/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
-	"os"
 
 	"github.com/Azure/applicationhealth-extension-linux/internal/cmdhandler"
 	"github.com/Azure/applicationhealth-extension-linux/internal/exithelper"
@@ -21,7 +19,6 @@ var (
 )
 
 func main() {
-
 	lg.With("version", version.VersionString())
 
 	cmdKey, err := cmdhandler.ParseCmd() // parse command line arguments
@@ -56,43 +53,7 @@ func main() {
 
 	err = handler.Execute(hEnv, seqNum) // execute the command
 	if err != nil {
-		lg.Error("failed to execute command", err)
+		lg.Error("failed to execute command", slog.Any("error", err))
 	}
 	lg.Info("end")
-}
-
-// parseCmd looks at os.Args and parses the subcommand. If it is invalid,
-// it prints the usage string and an error message and exits with code 0.
-func parseCmd(args []string) cmd {
-	if len(os.Args) != 2 {
-		printUsage(args)
-		err := fmt.Errorf("failed to parse command line arguments, expected 2, got %d", len(os.Args))
-		lg.Error("Incorrect usage", slog.Any("error", err))
-		os.Exit(2)
-	}
-	op := os.Args[1]
-	cmd, ok := cmds[op]
-	if !ok {
-		printUsage(args)
-		err := fmt.Errorf("failed to parse command line arguments, command not found: %s", op)
-		lg.Error(fmt.Sprintf("Incorrect command: %q\n", op), slog.Any("error", err))
-		os.Exit(2)
-	}
-	return cmd
-}
-
-// printUsage prints the help string and version of the program to stdout with a
-// trailing new line.
-func printUsage(args []string) {
-	fmt.Printf("Usage: %s ", os.Args[0])
-	i := 0
-	for k := range cmds {
-		fmt.Printf(k)
-		if i != len(cmds)-1 {
-			fmt.Printf("|")
-		}
-		i++
-	}
-	fmt.Println()
-	fmt.Println(version.DetailedVersionString())
 }
