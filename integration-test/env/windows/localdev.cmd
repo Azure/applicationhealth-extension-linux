df437ea80b5d2137c6303a8cf6a1fed2b3d2a7ed
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-03-13 22:18:27 +0000

    Add Windows environment configuration files. - HandlerEnviroment.windows.json is the Windows test HandlerEnviroment.json - localdev.cmd script to be ran before starting AppHealth-extension on Windows - localdev.settings is the testing .settings file for Windows.

---
+++ integration-test/env/windows/localdev.cmd
@@ -0,0 +1,14 @@
+REM BOOTSTRAP files that are needed
+
+REM set up logs, config etc
+IF NOT EXIST C:\temp\applicationhealth-extension MKDIR C:\temp\applicationhealth-extension
+IF NOT EXIST C:\temp\applicationhealth-extension\config MKDIR C:\temp\applicationhealth-extension\config
+IF NOT EXIST C:\temp\applicationhealth-extension\events MKDIR C:\temp\applicationhealth-extension\events
+IF NOT EXIST C:\temp\applicationhealth-extension\logs MKDIR C:\temp\applicationhealth-extension\logs
+IF NOT EXIST C:\temp\applicationhealth-extension\status MKDIR C:\temp\applicationhealth-extension\status
+
+REM now create the fake config files
+ECHO. > C:\temp\applicationhealth-extension\123.status
+COPY /Y .\localdev.settings C:\temp\applicationhealth-extension\config\123.settings
+ECHO. >  C:\temp\applicationhealth-extension\123.txt
+
