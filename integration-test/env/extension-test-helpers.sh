06a2cf602ca17687689c08899e5b41b2f70c147d
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-03-18 20:20:13 +0000

    Added logFilePath as variable and removed comments.

--- integration-test/env/extension-test-helpers.sh
+++ integration-test/env/extension-test-helpers.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 set -uex
+logFilePath="/var/log/azure/Extension/force-kill-extension.txt"
 
 force_kill_apphealth() {
     app_health_pid=$(ps -ef | grep "applicationhealth-extension" | grep -v grep | grep -v tee | awk '{print $2}')
 
     if [ -z "$app_health_pid" ]; then
-        echo "Applicationhealth extension is not running" > /var/log/azure/Extension/force-kill-extension.txt
+        echo "Applicationhealth extension is not running" > $logFilePath
         return 0
     fi
-    echo "Killing the applicationhealth extension forcefully" > /var/log/azure/Extension/force-kill-extension.txt
+    echo "Killing the applicationhealth extension forcefully" >> $logFilePath
     kill -9 $app_health_pid
 
     output=$(check_running_processes)
     if [ "$output" == "Applicationhealth and VMWatch are not running" ]; then
         echo "$output" >> /var/log/azure/Extension/force-kill-extension.txt
-        echo "Successfully killed the apphealth extension" >> /var/log/azure/Extension/force-kill-extension.txt
-        echo "Successfully killed the VMWatch extension" >> /var/log/azure/Extension/force-kill-extension.txt
+        echo "Successfully killed the apphealth extension" >> $logFilePath
+        echo "Successfully killed the VMWatch extension" >> $logFilePath
     else
         echo "$output" >> /var/log/azure/Extension/force-kill-extension.txt
-        echo "Failed to kill the apphealth extension" >> /var/log/azure/Extension/force-kill-extension.txt
+        echo "Failed to kill the apphealth extension" >> $logFilePath
     fi
     
 }
 
 check_running_processes() {
-    # check if the applicationhealth extension is running
-    # echo "Checking if the AppHealth Extension and VMWatch are running"
     local output=$(ps -ef | grep -e "applicationhealth-extension" -e "vmwatch_linux_amd64" | grep -v grep | grep -v tee)
     if [ -z "$output" ]; then
         echo "Applicationhealth and VMWatch are not running"
