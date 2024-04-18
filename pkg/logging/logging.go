4a5e227d3db6716a6654676a039da4e396713e4e
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-04-18 05:06:49 +0000

    Add logging package with NopLogger implementation

---
+++ pkg/logging/logging.go
@@ -0,0 +1,66 @@
+package logging
+
+import (
+	"io"
+
+	"github.com/go-kit/log"
+)
+
+type NopLogger struct {
+	log.Logger
+}
+
+func NewNopLogger() *NopLogger {
+	return &NopLogger{
+		Logger: log.NewNopLogger(),
+	}
+}
+
+func (l NopLogger) Info(format string, v ...interface{}) {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l NopLogger) Warn(format string, v ...interface{}) {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l NopLogger) Error(format string, v ...interface{}) {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l NopLogger) ErrorFromStream(prefix string, streamReader io.Reader) {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l NopLogger) WarnFromStream(prefix string, streamReader io.Reader) {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l NopLogger) InfoFromStream(prefix string, streamReader io.Reader) {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (l NopLogger) Close() {
+	err := l.Log()
+	if err != nil {
+		panic(err)
+	}
+}
