999672484ed34a586e399284d14dc77cafa8779a
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-06-05 22:15:50 +0000

    - Fix Windows AppHealthBianary Name constant

--- plugins/vmwatch/constants_windows.go
+++ plugins/vmwatch/constants_windows.go
@@ -3,5 +3,5 @@ package vmwatch
 const (
 	VMWatchBinaryNameAmd64   = "vmwatch_windows_amd64.exe"
 	VMWatchBinaryNameArm64   = "vmwatch_windows_arm64.exe"
-	AppHealthBinaryNameArm64 = "applicationhealth-extension-windows-arm64.exe"
+	AppHealthBinaryNameArm64 = "AppHealthExtension-arm64.exe"
 )
