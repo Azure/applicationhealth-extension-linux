75dbfbd1721e530e333c326873e482b130a140b0
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-06-07 04:33:15 +0000

    remove unused constant.

--- plugins/vmwatch/constants.go
+++ plugins/vmwatch/constants.go
@@ -5,6 +5,5 @@ const (
 
 	VMWatchConfigFileName     = "vmwatch.conf"
 	VMWatchVerboseLogFileName = "vmwatch.log"
-	VMWatchDefaultTests       = "disk_io:outbound_connectivity:clockskew:az_storage_blob"
 	VMWatchMaxProcessAttempts = 3
 )
