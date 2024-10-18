87f577c5c941d491123eb885c6318040a39c6271
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-10-18 00:43:50 +0000

    Refactor cmdhandler package to use slog.Logger instead of custom Logger interface in LinuxCommandHandler.Execute

--- platform/cmdhandler/cmdhandler_linux.go
+++ platform/cmdhandler/cmdhandler_linux.go
@@ -10,8 +10,9 @@ import (
 	"syscall"
 
 	"github.com/Azure/applicationhealth-extension-linux/internal/handlerenv"
+	"github.com/Azure/applicationhealth-extension-linux/internal/seqno"
+	"github.com/Azure/applicationhealth-extension-linux/internal/telemetry"
 	global "github.com/Azure/applicationhealth-extension-linux/internal/utils"
-	"github.com/Azure/applicationhealth-extension-linux/pkg/logging"
 	"github.com/Azure/applicationhealth-extension-linux/pkg/status"
 	"github.com/Azure/applicationhealth-extension-linux/plugins/vmwatch"
 	"github.com/pkg/errors"
@@ -20,7 +21,7 @@ import (
 var extCommands = CommandMap{
 	InstallKey:   cmd{f: install, Name: InstallName, ShouldReportStatus: false, pre: nil, failExitCode: 52},
 	UninstallKey: cmd{f: uninstall, Name: UninstallName, ShouldReportStatus: false, pre: nil, failExitCode: 3},
-	EnableKey:    cmd{f: enable, Name: EnableName, ShouldReportStatus: true, pre: nil, failExitCode: 3},
+	EnableKey:    cmd{f: enable, Name: EnableName, ShouldReportStatus: true, pre: enablePre, failExitCode: 3},
 	UpdateKey:    cmd{f: noop, Name: UpdateName, ShouldReportStatus: true, pre: nil, failExitCode: 3},
 	DisableKey:   cmd{f: noop, Name: DisableName, ShouldReportStatus: true, pre: nil, failExitCode: 3},
 }
@@ -39,35 +40,37 @@ func (ch *LinuxCommandHandler) CommandMap() CommandMap {
 	return ch.commands
 }
 
-func (ch *LinuxCommandHandler) Execute(c CommandKey, h *handlerenv.HandlerEnvironment, seqNum int, lg logging.Logger) error {
+func (ch *LinuxCommandHandler) Execute(c CommandKey, h *handlerenv.HandlerEnvironment, seqNum uint, lg *slog.Logger) error {
 	// Getting command to execute
 	command, ok := ch.commands[c]
 	if !ok {
 		return errors.Errorf("unknown command: %s", c)
 	}
 
-	lg.With("operation", strings.ToLower(command.Name.String()))
-	lg.With("seq", strconv.Itoa(seqNum))
+	lg = lg.With("operation", strings.ToLower(command.Name.String()))
+	lg = lg.With("seq", strconv.Itoa(int(seqNum)))
+	slog.SetDefault(lg)
 
-	lg.Info("Starting AppHealth Extension")
-	lg.Info(fmt.Sprintf("HandlerEnvironment: %v", h))
+	telemetry.SendEvent(telemetry.InfoEvent, telemetry.MainTask, "Starting AppHealth Extension")
+	telemetry.SendEvent(telemetry.InfoEvent, telemetry.MainTask, fmt.Sprintf("HandlerEnvironment: %v", h))
 	// subscribe to cleanly shutdown
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
 		<-sigs
-		lg.Info("Received shutdown request")
+		telemetry.SendEvent(telemetry.InfoEvent, telemetry.MainTask, "Received shutdown request")
 		global.Shutdown = true
 		err := vmwatch.KillVMWatch(lg, vmwatch.VMWatchCommand)
 		if err != nil {
 			lg.Error("error when killing vmwatch", slog.Any("error", err))
+			telemetry.SendEvent(telemetry.ErrorEvent, telemetry.KillVMWatchTask, fmt.Sprintf("Error when killing vmwatch process, error: %s", err.Error()))
 		}
 	}()
 
 	if command.pre != nil {
-		lg.Info("pre-check")
+		telemetry.SendEvent(telemetry.InfoEvent, telemetry.MainTask, "Running Pre-check")
 		if err := command.pre(lg, seqNum); err != nil {
-			lg.Error("pre-check failed", slog.Any("error", err))
+			telemetry.SendEvent(telemetry.ErrorEvent, telemetry.MainTask, fmt.Sprintf("Pre-check failed: %v", err))
 			os.Exit(command.failExitCode)
 		}
 	}
