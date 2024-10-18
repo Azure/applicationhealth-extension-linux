f38e5a5be7259d4df7bf5b2e94c99a4e186156fc
Kevin Lugo Rosado <klugorosado@microsoft.com>
2024-10-18 00:49:33 +0000

    Refactor cmdhandler_linux_test.go to use slog.Logger and push telemetry events on the enablePre function

--- platform/cmdhandler/cmdhandler_linux_test.go
+++ platform/cmdhandler/cmdhandler_linux_test.go
@@ -1,9 +1,14 @@
 package cmdhandler
 
 import (
+	"log/slog"
+	"os"
 	"testing"
 
+	"github.com/Azure/applicationhealth-extension-linux/internal/seqno"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func Test_extCommands(t *testing.T) {
@@ -43,4 +48,59 @@ func Test_extCommands(t *testing.T) {
 	require.Equal(t, "Disable", disableCmd.Name.String(), "disable command name should be 'Disable'")
 	require.True(t, disableCmd.ShouldReportStatus, "disable command should report status")
 	require.Equal(t, 3, disableCmd.failExitCode, "disable command failExitCode should be 3")
+}
+
+func Test_enablePre(t *testing.T) {
+	var (
+		logger          = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+		seqNumToProcess uint
+		ctrl            = gomock.NewController(t)
+	)
+
+	mockSeqNumManager := seqno.NewMockSequenceNumberManager(ctrl)
+	t.Run("SaveSequenceNumberError_ShouldFail", func(t *testing.T) {
+		// seqNumToProcess = 0, mrSeqNum = 1
+		seqNumToProcess = 0
+		mockSeqNumManager.EXPECT().GetCurrentSequenceNumber(gomock.Any(), gomock.Any()).Return(uint(1), nil)
+		seqno.SetInstance(mockSeqNumManager)
+		err := enablePre(logger, seqNumToProcess)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "most recent sequence number 1 is greater than the requested sequence number 0")
+	})
+	t.Run("GetSequenceNumberIsGreaterThanRequestedSequenceNumber_ShouldFail", func(t *testing.T) {
+		// seqNumToProcess = 4, mrSeqNum = 8
+		seqNumToProcess = 4
+		mockSeqNumManager.EXPECT().GetCurrentSequenceNumber(gomock.Any(), gomock.Any()).Return(uint(8), nil)
+		seqno.SetInstance(mockSeqNumManager)
+		err := enablePre(logger, seqNumToProcess)
+		assert.Error(t, err)
+		assert.EqualError(t, err, "most recent sequence number 8 is greater than the requested sequence number 4")
+	})
+	t.Run("SequenceNumberisZero_Startup", func(t *testing.T) {
+		// seqNumToProcess = 0, mrSeqNum = 0
+		seqNumToProcess = 0
+		mockSeqNumManager.EXPECT().GetCurrentSequenceNumber(gomock.Any(), gomock.Any()).Return(uint(0), nil)
+		mockSeqNumManager.EXPECT().SetSequenceNumber(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+		seqno.SetInstance(mockSeqNumManager)
+		err := enablePre(logger, seqNumToProcess)
+		assert.NoError(t, err)
+	})
+	t.Run("SequenceNumberAlreadyProcessed", func(t *testing.T) {
+		// seqNumToProcess = 5, mrSeqNum = 5
+		seqNumToProcess = 5
+		seqno.SetInstance(mockSeqNumManager)
+		mockSeqNumManager.EXPECT().GetCurrentSequenceNumber(gomock.Any(), gomock.Any()).Return(uint(5), nil)
+		mockSeqNumManager.EXPECT().SetSequenceNumber(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+		err := enablePre(logger, seqNumToProcess)
+		assert.NoError(t, err)
+	})
+	t.Run("MostRecentSeqNumIsSmaller_ShouldPass", func(t *testing.T) {
+		// seqNumToProcess = 4, mrSeqNum = 2
+		seqNumToProcess = 4
+		mockSeqNumManager.EXPECT().GetCurrentSequenceNumber(gomock.Any(), gomock.Any()).Return(uint(2), nil)
+		mockSeqNumManager.EXPECT().SetSequenceNumber(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+		seqno.SetInstance(mockSeqNumManager)
+		err := enablePre(logger, seqNumToProcess)
+		assert.NoError(t, err)
+	})
 }
